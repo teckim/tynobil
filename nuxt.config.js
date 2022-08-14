c75b25e4f266ef4c800e40b9c871076675247db8
BHDHakim <hakim.bhd@gmail.com>
2022-08-14 13:21:01 +0300

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~plugins/vuesax', mode: 'client' }
+    // { src: '~plugins/vuesax', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
